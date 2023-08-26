30941905c72b56e6edb70df589795c6eeec06cef
syahrandywaskito <syahrandywaskito666@gmail.com>
2023-08-26 16:29:52 +0700

    first commit

---
+++ Test/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <ncurses.h>
+
+int main()
+{
+  std::cout << "Hello\n";
+
+  return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include <ncurses.h>
+
+int main()
+{
+  initscr();
+
+  noecho();
+
+  curs_set(0);
+  
+  while (getch() != 'q')
+  {
+    mvaddch(10, 20, '@');
+  }
+  
+
+  endwin();
+
+  return 0;
+}
